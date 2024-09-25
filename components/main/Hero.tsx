3c1af131ad942e7749af6eeec043316f163c24be
ShakishnaviM <shakishnavi27@gmail.com>
2024-09-25 20:40:51 +0530

    added hero section

---
+++ components/main/Hero.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import HeroContext from '../sub/HeroContext'
+
+
+const Hero = () => {
+  return (
+    <div className='relative flex flex-col h-full w-full'>
+         <video
+        autoPlay
+        muted
+        loop
+        className="rotate-180 absolute top-[-420px]  h-full w-full left-0 z-[1] object-cover "
+      >
+        <source src="/New Project.mp4" type="video/mp4" />
+      </video>
+      
+      <HeroContext/>
+    </div>
+  )
+}
+
+export default Hero
---
+++ components/sub/HeroContext.tsx
@@ -0,0 +1,101 @@
+"use client"
+
+import React from 'react'
+import {motion} from 'framer-motion'
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion'
+import { SparklesIcon } from '@heroicons/react/24/solid'
+import Image from 'next/image'
+
+
+const HeroContext = () => {
+  return (
+    <motion.div
+    initial="hidden"
+    animate="visible"
+    className="h-full flex flex-row items-center justify-center md:px-20 px-10 w-full z-[20]"
+    >
+        <div className="h-full w-full flex flex-col md:gap-5 justify-center md:m-auto m-0 text-start">
+            <div className="flex flex-col md:flex-row md:gap-5 gap-2 md:mt-[100px] mt-[100px]">        
+                <motion.div
+                    variants={slideInFromTop}
+                    className="Welcome-box py-[8px] px-[7px] border border-[#a2790d] opacity-[0.9]"
+                    >
+                    <SparklesIcon className="text-[#b48811] mr-[10px] h-5 w-5" />
+                    <h1 className="Welcome-text text-[13px]">
+                        DevOps Engineer
+                    </h1>
+                </motion.div>
+
+                <motion.div
+                variants={slideInFromTop}
+                className="Welcome-box py-[8px] px-[7px] border border-[#a2790d] opacity-[0.9]"
+                >
+                <SparklesIcon className="text-[#b48811] mr-[10px] h-5 w-5" />
+                <h1 className="Welcome-text text-[13px]">
+                    Fullstack Developer 
+                </h1>
+                </motion.div>
+
+                <motion.div
+                variants={slideInFromTop}
+                className="Welcome-box py-[8px] px-[7px] border border-[#a2790d] opacity-[0.9]"
+                >
+                <SparklesIcon className="text-[#b48811] mr-[10px] h-5 w-5" />
+                <h1 className="Welcome-text text-[13px]">
+                    UI/UX enthusiast
+                </h1>
+                </motion.div>
+            </div>
+
+
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-row gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <span>
+            Hello<br/>
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-orange-500">
+              
+              I am Shakishnavi
+            </span>
+          </span>
+        </motion.div>
+
+
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-white my-5 max-w-[800px]"
+        >
+          Fueled by a deep interest in turning ideas into innovative software solutions, 
+          I have developed a strong foundation in fullstack projects. 
+          In the process of building software solutions during my academic and personal projects, 
+          I have demonstrated strong problem-solving skills, an ability to work collaboratively in teams, 
+          and a commitment to continuous learning.
+        </motion.p>
+        <motion.a
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+        >
+          Contact Me
+        </motion.a>
+        </div>
+
+
+        <motion.div
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <Image
+          src="/mainIconsdark.svg"
+          alt="work icons"
+          height={650}
+          width={650}
+        />
+      </motion.div>
+    </motion.div>
+
+    
+  )
+}
+
+export default HeroContext
