531fb0167cbd84303f58ca32244515dc9d2ee183
ShakishnaviM <shakishnavi27@gmail.com>
2024-09-27 20:28:04 +0530

    add config file

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+module.exports = {
+    output: 'export',  // This enables Next.js export
+    images: {
+      unoptimized: true,  // If you are using Next.js image optimization
+    },
+  };
+  
